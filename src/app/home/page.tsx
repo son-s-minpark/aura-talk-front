268f6e13f8a0ea8ae7ec8f96c404e34a46d8b5c5
minhong1126 <mjhong159357@gmail.com>
2025-03-17 15:09:02 +0900

    feat: 홈 화면 컴포넌트 통합, 클릭 시 색 변경 구현 #5

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,18 +1,77 @@
-import HomeHeader from "@/components/header/HomeHeader";
-import MainBox from "@/components/home/MainBox";
-import Nav from "@/components/home/Nav";
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import {
+  IoSearch,
+  IoNotifications,
+  IoChatbubbles,
+  IoPeople,
+  IoSettings,
+} from "react-icons/io5";
+import Image from "next/image";
+import logo from "../../../public/images/logo-none.png";
+import clsx from "clsx";
 
-const page = () => {
+type ListType = "chat" | "friend" | "setting";
+
+const Page = () => {
+  const [list, setList] = useState<ListType>("chat");
   return (
-    <div className="w-full h-full bg-black flex flex-col justify-between">
-      <HomeHeader />
-      <div>
-        <MainBox />
-        <Nav />
+    <div className="w-full h-full bg-black flex flex-col flex justify-between">
+      <div className="h-[76px] w-full items-center flex justify-between">
+        <button className="ml-[13px]">
+          <Image
+            src={logo}
+            alt="아우라 톡 로고"
+            className="w-[41px] h-[41px]"
+          />
+        </button>
+        <div className="text-white mr-[21px]">
+          <button>
+            <IoSearch className="w-[24px] h-[24px] mr-[12px]" />
+          </button>
+          <button>
+            <IoNotifications className="w-[24px] h-[24px]" />
+          </button>
+        </div>
+      </div>
+
+      <div className="dark:bg-darkBg bg-white rounded-t-[20px]">
+        <div className="h-[543px] w-full"></div>
+
+        <div className="w-full h-[48px] border-t-[1px] border-[#999999] flex items-center justify-center">
+          <div className="w-[301px] h-[29px] flex items-center justify-between">
+            <button
+              className={clsx({
+                "text-[#797C7B] dark:text-[#BCBCBC]": list != "chat",
+                "text-[#6EABFF] dark:text-[#8045FF]": list == "chat",
+              })}
+              onClick={() => setList("chat")}
+            >
+              <IoChatbubbles className="w-[30px] h-[30px]" />
+            </button>
+            <button
+              className={clsx({
+                "text-[#797C7B] dark:text-[#BCBCBC]": list != "friend",
+                "text-[#6EABFF] dark:text-[#8045FF]": list == "friend",
+              })}
+              onClick={() => setList("friend")}
+            >
+              <IoPeople className="w-[30px] h-[30px]" />
+            </button>
+            <button
+              className={clsx({
+                "text-[#797C7B] dark:text-[#BCBCBC]": list != "setting",
+                "text-[#6EABFF] dark:text-[#8045FF]": list == "setting",
+              })}
+              onClick={() => setList("setting")}
+            >
+              <IoSettings className="w-[30px] h-[30px]" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default page;
+export default Page;
--- src/components/home/MainBox.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const MainBox = () => {
-  return (
-    <div className="h-[543px] w-full dark:bg-darkBg bg-white rounded-t-[20px]"></div>
-  );
-};
-
-export default MainBox;
