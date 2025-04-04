709f12a0631c62f5f3e1aa2a7fcd5480a0b45a5f
minhong1126 <mjhong159357@gmail.com>
2025-04-04 23:37:19 +0900

    feat: 이용 약관 추가 #12

--- src/components/onboarding/modal/TermsModal.tsx
+++ src/components/onboarding/modal/TermsModal.tsx
@@ -1,13 +1,16 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import Checkbox from "@/components/common/CheckBtn";
+import CheckBtn from "@/components/common/CheckBtn";
 import SelectBtn from "@/components/common/SelectBtn";
 import { useRouter } from "next/navigation";
+
+type CheckType = "all" | "fir" | "sec" | "thr";
 
 const TermsModal = () => {
   const [isAllChecked, setIsAllChecked] = useState<boolean>(false);
   const [isFirChecked, setIsFirChecked] = useState<boolean>(false);
   const [isSecChecked, setIsSecChecked] = useState<boolean>(false);
+  const [isThrChecked, setIsThrChecked] = useState<boolean>(false);
   const router = useRouter();
 
   function handleIsAllChecked() {
@@ -15,23 +18,26 @@ const TermsModal = () => {
     setIsAllChecked(newIsAllChecked);
     setIsFirChecked(newIsAllChecked);
     setIsSecChecked(newIsAllChecked);
+    setIsThrChecked(newIsAllChecked);
   }
 
-  const handleCheckboxChange = (checkbox: "fir" | "sec", checked: boolean) => {
-    if (checkbox === "fir") {
+  const handlecheckChange = (check: CheckType, checked: boolean) => {
+    if (check === "fir") {
       setIsFirChecked(checked);
-    } else if (checkbox === "sec") {
+    } else if (check === "sec") {
       setIsSecChecked(checked);
+    } else if (check === "thr") {
+      setIsThrChecked(checked);
     }
   };
 
   useEffect(() => {
-    if (isFirChecked && isSecChecked) {
+    if (isFirChecked && isSecChecked && isThrChecked) {
       setIsAllChecked(true);
     } else {
       setIsAllChecked(false);
     }
-  }, [isFirChecked, isSecChecked]);
+  }, [isFirChecked, isSecChecked, isThrChecked]);
 
   function onSubmit() {
     if (isAllChecked) {
@@ -44,47 +50,56 @@ const TermsModal = () => {
   return (
     <div
       onClick={(e) => e.stopPropagation()}
-      className="modal-content bg-[#2A2A2A] text-white h-[207px] w-[333px]"
+      className="modal-content bg-[#2A2A2A] text-white h-[226px] w-[333px]"
     >
       <div
         className="h-[67px] w-full border-b-1 border-[var(--color-commonGray)] px-[24px] pt-[25px] pb-[22px] flex items-center"
         onClick={handleIsAllChecked}
       >
-        <Checkbox isChecked={isAllChecked} />
-        <div className="w-full flex justify-between ml-[16px]">
+        <CheckBtn isChecked={isAllChecked} />
+        <div className="w-full flex justify-between items-center ml-[16px]">
           <p className="text-[20px] font-bold">약관 전제 동의</p>
+          <p className="text-[#999999] text-[10px] underline">내용 보기</p>
         </div>
       </div>
 
       <div className="flex flex-col gap-[9px] px-[24px]">
         <div
           className="flex pt-[27px]"
-          onClick={() => handleCheckboxChange("fir", !isFirChecked)}
+          onClick={() => handlecheckChange("fir", !isFirChecked)}
         >
-          <Checkbox isChecked={isFirChecked} />
-          <div className="w-full flex justify-between items-center ml-[16px]">
+          <CheckBtn isChecked={isFirChecked} />
+          <div className="w-full flex items-center ml-[16px]">
             <p className="text-[14px] font-medium">
-              이용약관에 동의합니다. (필수)
+              개인정보 약관에 동의합니다. (필수)
             </p>
-            <p className="text-[#999999] text-[10px] underline">내용 보기</p>
           </div>
         </div>
 
         <div
           className="flex"
-          onClick={() => handleCheckboxChange("sec", !isSecChecked)}
+          onClick={() => handlecheckChange("sec", !isSecChecked)}
         >
-          <Checkbox isChecked={isSecChecked} />
-          <div className="w-full flex justify-between items-center ml-[16px]">
+          <CheckBtn isChecked={isSecChecked} />
+          <div className="w-full flex items-center ml-[16px]">
             <p className="text-[14px] font-medium">
-              서비스약관에 동의합니다. (필수)
+              서비스 약관에 동의합니다. (필수)
             </p>
-            <p className="text-[#999999] text-[10px] underline">내용 보기</p>
+          </div>
+        </div>
+
+        <div
+          className="flex"
+          onClick={() => handlecheckChange("thr", !isThrChecked)}
+        >
+          <CheckBtn isChecked={isThrChecked} />
+          <div className="w-full flex items-center ml-[16px]">
+            <p className="text-[14px] font-medium">14세 이상입니다. (필수)</p>
           </div>
         </div>
       </div>
 
-      <div className="mb-[21px] mr-[19px] mt-[25px] flex justify-end">
+      <div className="mb-[18px] mr-[19px] mt-[16px] flex justify-end">
         <SelectBtn label="완료" onClick={onSubmit} />
       </div>
     </div>
