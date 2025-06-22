70b8cf60b6eb28cf4ce3872279555feac70fb4cc
minhong1126 <mjhong159357@gmail.com>
2025-06-22 15:41:02 +0900

    feat: 요청 중인 친구 목록 요청 구현 #21

--- src/components/friend/RequestingList.tsx
+++ src/components/friend/RequestingList.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ModalFriendComponent from "./ModalFriendComponent";
 import SelectBtn from "../common/SelectBtn";
-
-const data = [1, 2, 3, 4, 5];
+import { useRequestedFriend } from "@/hooks/friend/useRequestedFriend";
+import { friendType } from "@/type/friend/friendType";
 
 const RequestingList = () => {
+  const { useGetRequestingFriendList } = useRequestedFriend();
+
+  const { data, isLoading } = useGetRequestingFriendList();
   function onCancelFriendRequest() {}
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-      {data.map((friend) => (
-        <div className="flex justify-between" key={friend}>
-          <ModalFriendComponent id={friend} />
+      {data.map((friend: friendType) => (
+        <div className="flex justify-between" key={friend.id}>
+          <ModalFriendComponent friend={friend} />
           <div className="flex gap-[6px] items-center">
             <SelectBtn
               isRejected={true}
--- src/hooks/friend/useRequestedFriend.ts
+++ src/hooks/friend/useRequestedFriend.ts
@@ -23,5 +23,26 @@ export const useRequestedFriend = () => {
           });
       },
     });
-  return { useGetWaitingFriendList };
+
+  const useGetRequestingFriendList = () =>
+    useQuery({
+      queryKey: ["getRequestingFriendList"],
+      queryFn: async () => {
+        return await axiosInstance
+          .get(apiRoute.FREIND_REQUEST_SENT_LIST)
+          .then((res) => {
+            const data = res.data;
+            if (data.success) {
+              return data.data;
+            } else {
+              throw new Error("받은 친구 요청 가져오기 에러");
+            }
+          })
+          .catch((err) => {
+            throw new Error(err);
+          });
+      },
+    });
+
+  return { useGetWaitingFriendList, useGetRequestingFriendList };
 };
